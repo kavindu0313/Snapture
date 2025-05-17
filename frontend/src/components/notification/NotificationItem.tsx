893fb2c7b6267e50282ef937f900a00347abf97b
Tharindu Nuwantha <105153249+TharinduNuwantha@users.noreply.github.com>
2025-05-17 18:44:41 +0530

    comment
    comment

--- frontend/src/components/notification/NotificationItem.tsx
+++ frontend/src/components/notification/NotificationItem.tsx
@@ -55,6 +55,7 @@ const NotificationItem: React.FC<NotificationItemProps> = ({ notification, onMar
           </div>
         );
       case 'comment':
+        //comment
         return (
           <div className="flex items-center justify-center w-10 h-10 bg-blue-100 rounded-full">
             <svg
