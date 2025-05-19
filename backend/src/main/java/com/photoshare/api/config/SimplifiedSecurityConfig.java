bd840890683571e7f6ae4d11a60a01bd8132b5df
Kavindu Senanayake <gihankavindu789@gmail.com>
2025-05-19 10:42:35 +0530

    Delete backend directory

--- backend/src/main/java/com/photoshare/api/config/SimplifiedSecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.photoshare.api.config;
-/**
- * This class has been intentionally left empty.
- * The original implementation has been moved to SimpleSecurityConfig.java
- * to avoid bean definition conflicts.
- */
-public class SimplifiedSecurityConfig {
-    // Intentionally empty
-}
--- backend/start-backend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting PhotoShare Backend...
-cd %~dp0
-call mvn spring-boot:run > backend-log.txt 2>&1
